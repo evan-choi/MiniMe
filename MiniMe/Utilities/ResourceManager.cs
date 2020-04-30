1b4b9b1f3a25a0125e7fd4604c33b3b9def3efa5
evan-choi <evan@chequer.io>
2020-05-01 02:37:08 +0900

    Add public key infrastructure

---
+++ MiniMe/Utilities/ResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace MiniMe.Utilities
+{
+    internal static class ResourceManager
+    {
+        private static readonly Assembly _assembly = typeof(ResourceManager).Assembly;
+
+        private static string GetFullResourceName([NotNull] string resourceName)
+        {
+            return $"MiniMe.Resources.{resourceName}";
+        }
+
+        public static Stream GetResourceStream([NotNull] string resourceName)
+        {
+            resourceName = GetFullResourceName(resourceName);
+            return _assembly.GetManifestResourceStream(resourceName);
+        }
+    }
+}
