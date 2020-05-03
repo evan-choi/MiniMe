51e02026f39391e4664351722d3691aed9e11e1d
Evan <evan@chequer.io>
2020-05-03 22:21:41 +0900

    Remove old migration script

--- MiniMe.Aime/Data/Tools/Initialize.ps1
+++
@@ -1,4 +0,0 @@
-﻿Set-Location (Get-Item $PSScriptRoot).Parent.Parent.FullName
-
-Remove-Item -Recurse -Force -ErrorAction Ignore ./Data/Migrations
-dotnet ef migrations add InitialCreate -o ./Data/Migrations
